4a0d1f313a4692f966677aff9894d5ab2718c6f0
“Matheus-oliKelvin” <matheuskelvin309@gmail.com>
2026-05-04 20:01:17 -0300

    verificação se o e-mail já existe

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ while not login[0]:
     while True:
         print("\n Selecione a opção desejada: ")
         print("1- Login") 
-        print("2- Cadastra-se como cliente ")
+        print("2- Cadastra-se  ")
         print("0- Sair")
 
         opcao= int(input(" digite a opção : "))
@@ -31,7 +31,7 @@ while not login[0]:
         encontrou= False
 
         for usuario in usuarios:
-            if email == usuario[0] and senha == usuario[1] and tipo == usuario[2]: #!perguntar se verifica o tipo
+            if email == usuario[0] and senha == usuario[1] and tipo == usuario[2]:
                 print("Seja bem-vindo(a) !")
                 login=[True,tipo]
                 encontrou=True
@@ -45,10 +45,27 @@ while not login[0]:
 
         while True: 
             email= input("Digite seu e-mail: ")
+            
+            for usuario in usuarios:
+                if usuario[0] == email:
+                    print("Esse e-mail já está cadastrado!Digite novamente")
+                    verificador= False
+                    break
+                else:
+                    verificador=True
+
+
             if "@" in email:
-                break
+                verificador= True
             else:
-                print("E-mail invalido, digite novamente: ")
+                print("email inválido, falta o @:")
+                verificador=False
+                break
+
+
+            if verificador :
+                    break
+
 
         while True:
             senha= input("Digite sua senha : ")
