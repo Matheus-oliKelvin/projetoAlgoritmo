9ba5ea45003f6126f83148eafd077e82f2aaa224
“Matheus-oliKelvin” <matheuskelvin309@gmail.com>
2026-05-04 19:39:07 -0300

    criação da função login

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
 
-while True:
+usuarios= []
+login=[False, None]
+
+while not login[0]:
+
     while True:
         print("\n Selecione a opção desejada: ")
-        print("1- login")
-        print("2- Cadastra-se")
-        print("3- Sair")
+        print("1- Login") 
+        print("2- Cadastra-se como cliente ")
+        print("0- Sair")
+
         opcao= int(input(" digite a opção : "))
+
         if opcao >=1 and opcao<=3:
             break
         else:
             print("Opção invalida! Digite novamente\n")
-    if opcao == 3:
+
+
+    if opcao == 0:
         print("Saindo...")
         break
+
+    elif opcao == 1:
+        email= input("Digite seu e-mail: ")
+        senha= input("Digite sua senha: ")
+        tipo= input("Deseja entrar como admin ou cliente?: ").lower()
+
+       
+        encontrou= False
+
+        for usuario in usuarios:
+            if email == usuario[0] and senha == usuario[1] and tipo == usuario[2]: #!perguntar se verifica o tipo
+                print("Seja bem-vindo(a) !")
+                login=[True,tipo]
+                encontrou=True
+                break
+
+        if not encontrou:
+            print("E-mail ou senha incorretos ")
+
+
     elif opcao == 2:
+
         while True: 
             email= input("Digite seu e-mail: ")
             if "@" in email:
                 break
             else:
                 print("E-mail invalido, digite novamente: ")
+
         while True:
             senha= input("Digite sua senha : ")
             confimarSenha= input("Digite sua senha novamente: ")
@@ -27,12 +57,15 @@ while True:
                 break
             else:
                 print("As senhas não coecidem")
+
         while True:
             tipo= input("Você deseja se cadastrar como um CLIENTE ou ADM").lower()
             if tipo == "cliente" or tipo =="adm":
                 break
             else:
                 print("Nome invalido, tente novamente")
-        usuarios= []
-        usuarios.append(email, senha, tipo)
+
+        
+        usuarios.append([email, senha, tipo]) 
         print("Usuário cadastrado com sucesso!")
+
