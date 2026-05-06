fb639d8766803a65a15220a4c35143591bf2a116
Pepeu2 <joaopedro20082008jp@gmail.com>
2026-05-05 21:17:04 -0300

    vou adicionar a opçao de retirada

--- main.py
+++ main.py
@@ -3,153 +3,222 @@ animais = [["vacas", 1000, 5], ["leitoes", 300, 5], ["ovelhas", 350, 5]]
 
 total_compra = 0
 
-usuarios= [["a", "a", "cliente", 1 ]]
-login=[False, None]
+usuarios = [["a", "a", "cliente"]]
+login = [False, None]
+
+agendar_data = [[10.04, 11,.04]]
+agendar_hora = [[12.22, 12.23]]
+
 
 while not login[0]:
 
     while True:
         print("\n Selecione a opção desejada: ")
         print("1- Login") 
-        print("2- Cadastra-se  ")
+        print("2- Cadastra-se")
         print("0- Sair")
 
-        opcao= int(input(" digite a opção : "))
+        opcao = int(input(" digite a opção : "))
 
-        if opcao >=1 and opcao<=3:
+        if opcao >= 1 and opcao <= 3:
             break
         else:
             print("Opção invalida! Digite novamente\n")
-
 
     if opcao == 0:
         print("Saindo...")
         break
 
     elif opcao == 1:
-        email= input("Digite seu e-mail: ")
-        senha= input("Digite sua senha: ")
-        tipo= input("Deseja entrar como admin ou cliente?: ").lower()
+        email = input("Digite seu e-mail: ")
+        senha = input("Digite sua senha: ")
+        tipo = input("Deseja entrar como admin ou cliente?: ").lower()
 
-       
-        encontrou= False
+        encontrou = False
 
         for usuario in usuarios:
             if email == usuario[0] and senha == usuario[1] and tipo == usuario[2]:
                 print("Seja bem-vindo(a) !")
-                login=[True,tipo]
-                encontrou=True
+                login = [True, tipo]
+                encontrou = True
                 break
 
         if not encontrou:
             print("E-mail ou senha incorretos ")
 
-
     elif opcao == 2:
 
-        while True: 
-            email= input("Digite seu e-mail: ")
-            
+        while True:
+            email = input("Digite seu e-mail: ")
+
             for usuario in usuarios:
                 if usuario[0] == email:
                     print("Esse e-mail já está cadastrado!Digite novamente")
-                    verificador= False
+                    verificador = False
                     break
                 else:
-                    verificador=True
-
+                    verificador = True
 
             if "@" in email:
-                verificador= True
+                verificador = True
             else:
                 print("email inválido, falta o @:")
-                verificador=False
+                verificador = False
                 break
 
-
-            if verificador :
-                    break
-
+            if verificador:
+                break
 
         while True:
-            senha= input("Digite sua senha : ")
-            confimarSenha= input("Digite sua senha novamente: ")
+            senha = input("Digite sua senha : ")
+            confimarSenha = input("Digite sua senha novamente: ")
+
             if senha == confimarSenha:
                 break
             else:
                 print("As senhas não coecidem")
 
         while True:
-            tipo= input("Você deseja se cadastrar como um CLIENTE ou ADM").lower()
-            if tipo == "cliente" or tipo =="adm":
+            tipo = input("Você deseja se cadastrar como um CLIENTE ou ADM").lower()
+
+            if tipo == "cliente" or tipo == "adm":
                 break
             else:
                 print("Nome invalido, tente novamente")
 
-        
-        usuarios.append([email, senha, tipo]) 
+        usuarios.append([email, senha, tipo])
         print("Usuário cadastrado com sucesso!")
 
 while login[0] and login[1] == "cliente":
-    
-    
-    
-    print ("1 - ver produtos")
-    print ("2 - ver animais")
-    print ("3 - comprar")
-    print ("4 - agendar retirada")
-    print ("5 - ver total da compra")
-    print ("#########")
-    print ("0 - sair")
 
-    opcao = input ("diga uma opção valida: ")
+    print("\n1 - ver produtos")
+    print("2 - ver animais")
+    print("3 - comprar")
+    print ("4 - agendar retirada")
+    print("5 - valor total das compras")
+    print("0 - sair")
+    
+    opcao = input("digite uma opção: ")
 
     if opcao == "1":
-        for produtos in produtos:
-            print ("Nome:", produtos[0])
-            print ("Preço:", produtos[1],"$")
-            print ("Estoque:", produtos[2], "em estoque")
-            print ("\n")    
+        for produto in produtos:
+            print ("Nome:", produto[0]) 
+            print ("Preço:", produto[1],"$") 
+            print ("Estoque:", produto[2], "em estoque") 
+            print ("\n")
     
     elif opcao == "2":
-        for animais in animais:
-            print ("Nome:", animais[0])
-            print ("Preço:", animais[1],"$")
-            print ("Estoque:", animais[2], "em estoque")
-            print ("\n")  
+        for animal in animais:
+            print ("Nome:", animal[0]) 
+            print ("Preço:", animal[1],"$") 
+            print ("Estoque:", animal[2], "em estoque") 
+            print ("\n")
     
     elif opcao == "3":
         compra = input("O que deseja comprar: ").lower().strip()
         encontrado = False
 
-    for produto in produtos:
-        if compra == produto[0]:
-            encontrado = True
-            quantidade = int(input("Quantidade: "))
-
-            if produto[2] >= quantidade:
-                produto[2] -= quantidade
-                valor = produto[1] * quantidade
-                total_compra += valor
-                print("Compra realizada! Total:", valor)
-            else:
-                print("Estoque insuficiente")
-            break
-
-    if not encontrado:
-        for animal in animais:
-            if compra == animal[0]:
+        for produto in produtos:
+            if compra == produto[0]:
                 encontrado = True
-                quantidade = int(input("Quantidade: "))
+                quantidade = int(input("diga a quantidade que deseja comprar: "))
 
-                if animal[2] >= quantidade:
-                    animal[2] -= quantidade
-                    valor = animal[1] * quantidade
+                if produto[2] >= quantidade:
+                    produto[2] -= quantidade
+                    valor = produto[1] * quantidade
                     total_compra += valor
-                    print("Compra realizada! Total:", valor)
+                    print(f"o valor da compra foi de: R$ {valor}")
                 else:
-                    print("Estoque insuficiente")
+                    print("estoque insuficiente!")
                 break
 
-    if not encontrado:
-        print("Produto não encontrado")
+        if not encontrado:
+            for animal in animais:
+                if compra == animal[0]:
+                    encontrado = True
+                    quantidade = int(input("diga a quantidade que deseja comprar: "))
+
+                    if animal[2] >= quantidade:
+                        animal[2] -= quantidade
+                        valor = animal[1] * quantidade
+                        total_compra += valor
+                        print(f"o valor da compra foi de: R$ {valor}")
+                    else:
+                        print("estoque insuficiente!")
+                    break
+
+        if not encontrado:
+            print("Não encontrado")
+
+    
+    elif opcao == "4":
+        data = (input ("qual a data que deseja fazer a retirada: ex (12.03): "))
+        hora = (input ("qual a hora que deseja fazer a retirada: ex (15.55): "))
+
+        agendar_data.append (data)
+        agendar_hora.append (hora)
+    
+        if data in agendar_data:
+            print ("data indisponivel. escolha outra!")
+        else:
+            print (f"data agendada! para o dia {data} as {hora} ")
+        if hora in agendar_hora:
+            print ("")
+    
+    
+    
+    
+    
+    
+    elif opcao == "5":
+        print(f"o total da suas compras foram de: R${total_compra}" )
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    elif opcao == "0":
+        break
